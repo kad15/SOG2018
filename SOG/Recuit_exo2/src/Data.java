a3edc4455ca32ba0dfbe84cc50576f0403769e1d
kad15 <kader.beldjilali@free.fr>
2018-11-16 23:40:15 +0100

    divers

--- SOG/Recuit_exo2/src/Data.java
+++ SOG/Recuit_exo2/src/Data.java
@@ -1,101 +1,93 @@
-import java.util.*;
+import java.util.Random;
 
 public class Data {
 
-    public static double tabPoids[];
-    public static double tabValeurs[];
-    public static double[][] tabVilles;
-    public static int tabAvions[];
-    private static Random generateur=new Random(123);
+	public static double tabPoids[];
+	public static double tabValeurs[];
+	public static double[][] tabVilles;
+	public static int tabAvions[];
+	private static Random generateur = new Random(123);
 
+	/************************************************************/
+	/* parametres de generation */
+	private static double RAYON = 100.0;
+	private static int POIDS = 100;
+	private static int VALEURS = 100;
+	/************************************************************/
 
-    /************************************************************/
-    /*   parametres de generation                               */
-    private static double RAYON=100.0;
-    private static int POIDS=100;
-    private static int VALEURS=100;
-    /************************************************************/
+	public static int separation[][] = { { 4, 5, 6 }, { 3, 3, 4 }, { 3, 3, 3 } };
 
-
-    public static int separation[][]={{4,5,6},
-                                      {3,3,4},
-                                      {3,3,3}};
-
-
-    // generation de n objets pour le probleme du sac a dos
-    public static void genererObjets(int n) {
-		tabPoids=new double[n];
-		tabValeurs=new double[n];
-		for (int i=0;i<n;i++){
-		    tabPoids[i]=(double)(1+generateur.nextInt(POIDS));
-		    tabValeurs[i]=(double)(1+generateur.nextInt(VALEURS));
+	// generation de n objets pour le probleme du sac a dos
+	public static void genererObjets(int n) {
+		tabPoids = new double[n];
+		tabValeurs = new double[n];
+		for (int i = 0; i < n; i++) {
+			tabPoids[i] = 1 + generateur.nextInt(POIDS);
+			tabValeurs[i] = 1 + generateur.nextInt(VALEURS);
 		}
-    }
-
-    // affichage des objets
-    public static void afficherObjets() {
-	System.out.println("***************************** Objets******************************");
-		for (int i=0;i<tabPoids.length;i++){
-		    System.out.println("Objet " + i + " poids= "+ tabPoids[i] + " valeur=" + tabValeurs[i]);
-		}
-    }
-
-
-    // generation de n villes sur le cercle (TSP)
-    public static void genererVillesCercle(int n) {
-	double theta,x,y;
-	tabVilles=new double[n][2];
-		for (int i=0;i<n;i++){
-		    theta=2*Math.PI*generateur.nextDouble();
-		    x=RAYON*Math.cos(theta);
-	        y=RAYON*Math.sin(theta);
-		    
-		    tabVilles[i][0]=x;
-		    tabVilles[i][1]=y;
-		}
-    }
-
-
-    // generation de n villes dans un carre RAYONxRAYON (TSP)
-    public static void genererVilles(int n) {
-	double x,y;
-
-	tabVilles = new double[n][2];
-		for (int i=0;i<n;i++){
-		    x=RAYON*generateur.nextDouble();
-		    y=RAYON*generateur.nextDouble();
-		    tabVilles[i][0]=x;
-		    tabVilles[i][1]=y;
-		}
-    }
-
-    // affichage des villes
-    public static void afficherVilles() {
-		System.out.println("***************************** Villes ******************************");
-		for (int i=0;i<tabVilles.length;i++){
-		    System.out.println("Ville " + i + " x= "+ tabVilles[i][0]+ " y= " + tabVilles[i][1]);
-		}
-    }
-
-    // generation de n avions (classe heavy(0), Medium(1), Small(2))
-    public static void genererAvions(int n) {
-	tabAvions=new int[n];
-	for (int i=0;i<n;i++){
-		    tabAvions[i]=generateur.nextInt(3);
-		}
-    }
-
-    // affichage sequence avions
-    public static void afficherAvions() {
-	System.out.println("***************************** Avions ******************************");
-	for (int i=0;i<tabAvions.length;i++){
-	    System.out.println("Avion " + i + " classe= "+ tabAvions[i]);
 	}
-    }
 
+	// affichage des objets
+	public static void afficherObjets() {
+		System.out.println("***************************** Objets******************************");
+		for (int i = 0; i < tabPoids.length; i++) {
+			System.out.println("Objet " + i + " poids= " + tabPoids[i] + " valeur=" + tabValeurs[i]);
+		}
+	}
 
-    public static void main(String[] args){
-		int dim=100;
+	// generation de n villes sur le cercle (TSP)
+	public static void genererVillesCercle(int n) {
+		double theta, x, y;
+		tabVilles = new double[n][2];
+		for (int i = 0; i < n; i++) {
+			theta = 2 * Math.PI * generateur.nextDouble();
+			x = RAYON * Math.cos(theta);
+			y = RAYON * Math.sin(theta);
+
+			tabVilles[i][0] = x;
+			tabVilles[i][1] = y;
+		}
+	}
+
+	// generation de n villes dans un carre RAYONxRAYON (TSP)
+	public static void genererVilles(int n) {
+		double x, y;
+
+		tabVilles = new double[n][2];
+		for (int i = 0; i < n; i++) {
+			x = RAYON * generateur.nextDouble();
+			y = RAYON * generateur.nextDouble();
+			tabVilles[i][0] = x;
+			tabVilles[i][1] = y;
+		}
+	}
+
+	// affichage des villes
+	public static void afficherVilles() {
+		System.out.println("***************************** Villes ******************************");
+		for (int i = 0; i < tabVilles.length; i++) {
+			System.out.println("Ville " + i + " x= " + tabVilles[i][0] + " y= " + tabVilles[i][1]);
+		}
+	}
+
+	// generation de n avions (classe heavy(0), Medium(1), Small(2))
+	public static void genererAvions(int n) {
+		tabAvions = new int[n];
+		for (int i = 0; i < n; i++) {
+			tabAvions[i] = generateur.nextInt(3);
+		}
+	}
+
+	// affichage sequence avions
+	public static void afficherAvions() {
+		System.out.println("***************************** Avions ******************************");
+		for (int i = 0; i < tabAvions.length; i++) {
+			System.out.println("Avion " + i + " classe= " + tabAvions[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+		int dim = 100;
 		genererObjets(dim);
 		afficherObjets();
 		genererVillesCercle(dim);
@@ -104,5 +96,5 @@ public class Data {
 		afficherVilles();
 		genererAvions(dim);
 		afficherAvions();
-    }
+	}
 }
--- SOG/Recuit_exo2/src/EtatKP.java
+++ SOG/Recuit_exo2/src/EtatKP.java
@@ -1,85 +1,109 @@
 import java.util.Random;
+
 // recuit pour le problème du sac à dos
 public class EtatKP {
-	
+
 	public int dimEtat;
 	public int[] X;
-	
+
 	private static Random generateur = new Random(999);
-	private int oldIndexI;//, oldIndexJ;
-	//private double oldPoids;
+	private int oldIndexI;
 	private double capacite = 2000;
-	private double P, V;
-	
-	 /*******************************************************************************************************************/
-	  /* Methodes locales */
-	  /*******************************************************************************************************************/
-	//echange deux valeurs du vecteur d'etat X  
-	public void swap(int a, int b) {
-		  int tmp;
-		  tmp = X[a];
-		  X[a]=X[b];
-		  X[b]=tmp;	  
-	  }
-	
-	public double getP()
-	{ return P;}	
+	private double P, V; // p : somme des poids et V somme des valeurs,
+	// ce sont des attributs de la classe EtatKP initialisés
+	// dans la méthode EtatKP::calculCritere
+
 	/*******************************************************************************************************************/
-	  /* Constructeur */
-	  /*******************************************************************************************************************/
-	  public EtatKP(int dimEtat) {
-		  this.dimEtat = dimEtat;
-		  X = new int[dimEtat];
-	  }
-	  /*******************************************************************************************************************/
-	  /* Initialisation aleatoire de l'etat */
-	  /*******************************************************************************************************************/
-	  public void initAleatEtat() {
-		  for(int i= 0 ; i<dimEtat;i++)
-			  X[i]=generateur.nextInt(2);
-	  }
-	  /*******************************************************************************************************************/
-	  /* Evaluation des objectifs */
-	  /*******************************************************************************************************************/
-	  public double calculCritere() {
-		  double valeur = 0;
-		  double poids = 0;
-		  
-		  for (int i=0; i<dimEtat; ++i){
-	    		valeur += X[i] * Data.tabValeurs[i];
-	    		poids  += X[i] * Data.tabPoids[i];
-	    	}
-		  V = valeur;
-		  P = poids;
-		  double delta = poids - capacite;
-		  double epsilon = delta / capacite;
-	    	if (delta <= 0){
-	    		return valeur;
-	    	}
-	    	else{
-	    		//return valeur/(1+epsilon*epsilon);
-	    		return valeur -10*delta;
-	    		//return 0;
-	    	}
-	  }
-	  /*******************************************************************************************************************/
-	  /* GenererVoisin */
-	  /*******************************************************************************************************************/
-	  public void genererVoisin() {
-		  int indexI = generateur.nextInt(dimEtat); // on choisit un indice au hasard entre 0 et dimEtat-1
-		  X[indexI]=(X[indexI] + 1) % 2;// si X(i)= 0, 0+1=1%2 donne 1, si X[i]= 1, 1+1=2%2 donne 0
-		  oldIndexI=indexI; //on memorise l'index pour pouvoir faire un retour arriere le cas echeant
-	  }  
-	  /*******************************************************************************************************************/
-	  /* Retour a la solution precedente */
-	  /*******************************************************************************************************************/
-	  public void comeBack() {
-		  X[oldIndexI] = (X[oldIndexI] + 1) % 2; 
-	  }  
-	  /*******************************************************************************************************************/
-	  /* Affichage */
-	  /*******************************************************************************************************************/
-	  public void afficherEtat() {
-		  System.out.println("poids sac = " + P +" valeur sac =  " + V);
-	  }  
+	/* Methodes locales */
+	/*******************************************************************************************************************/
+	// echange deux valeurs du vecteur d'etat X
+	public void swap(int a, int b) {
+		int tmp;
+		tmp = X[a];
+		X[a] = X[b];
+		X[b] = tmp;
+	}
+	/*
+	 * public double getP() { return P; }
+	 */
+	/*******************************************************************************************************************/
+
+	/* Constructeur */
+	/*******************************************************************************************************************/
+	public EtatKP(int dimEtat) {
+		this.dimEtat = dimEtat;
+		X = new int[dimEtat];
+	}
+
+	/*******************************************************************************************************************/
+	/* Initialisation aleatoire de l'etat */
+	/*******************************************************************************************************************/
+	public void initAleatEtat() {
+		for (int i = 0; i < dimEtat; i++)
+			X[i] = generateur.nextInt(2);
+	}
+
+	/*******************************************************************************************************************/
+	/* Evaluation des objectifs */
+	/*******************************************************************************************************************/
+	public double calculCritere() {
+		double valeur = 0;
+		double poids = 0;
+
+		for (int i = 0; i < dimEtat; ++i) {
+			valeur += X[i] * Data.tabValeurs[i];
+			poids += X[i] * Data.tabPoids[i];
+		}
+		V = valeur;
+		P = poids;
+		double delta = poids - capacite; // dans le cas de débordement du sac qu'on cherche à éviter delta > 0
+		double epsilon = delta / capacite; // variable pour d'autres relaxations
+		// on cherche à maximiser valeur subject to constraint poids < capacité C du sac
+		// on relaxe le pb en choisissant un critère "y" qui va pénaliser les cas où les
+		// objets "débordent" du sac
+		// on pose donc delta = poids - capacité > 0 en cas de débordement ; c'est un
+		// choix
+		// fait pour manipuler un delta >0.
+		// ce delta étant nul si le sac n'est pas saturé ce qui implique dans ce cas
+		// simplement un critère = à la fct objectif : valeur
+		// on optimise en maximisation donc pénaliser la fonction objectif nécessite de
+		// lui soustraire une fonction de delta f(delta) à valeurs positives.
+		// test avec f(delta) = delta**2
+		if (delta < 0) { // cas où capacité > poids, on prend pour critère y = valeur + 0 = valeur = fct
+							// objectif
+			return valeur;
+		} else { // cas où la capa C est < au poids total des objets dans le sac
+					// return valeur/(1+epsilon*epsilon);
+			return valeur - delta * delta;
+			// return 0;
+		}
+	}
+
+	/*******************************************************************************************************************/
+	/* GenererVoisin */
+	/*******************************************************************************************************************/
+	// voisinnage = ensemble des états qui ne diffèrent que de 1 bit par rapport à
+	// xi donc #voisinnage = 2000.
+	public void genererVoisin() {
+		int indexI = generateur.nextInt(dimEtat); // on choisit un indice au hasard entre 0 et dimEtat-1
+		X[indexI] = (X[indexI] + 1) % 2;// si X(i)= 0, 0+1=1%2 donne 1, si X[i]= 1, 1+1=2%2 donne 0
+		oldIndexI = indexI; // on memorise l'index pour pouvoir faire un retour arriere le cas echeant
+	}
+
+	/*******************************************************************************************************************/
+	/* Retour a la solution precedente */
+	/*******************************************************************************************************************/
+	public void comeBack() {
+		X[oldIndexI] = (X[oldIndexI] + 1) % 2;
+// nb : les valeurs et les poids totaux sont quant à eux remis à jours à partir du vecteur X "old" ci-dessus
+// car dans la méthode refroidissement (dans la boucle for) calculCritere() est appelée qui utilise les X[i]
+// pour le calcul de "poids" et "valeur". 
+	}
+
+	/*******************************************************************************************************************/
+	/* Affichage */
+	/*******************************************************************************************************************/
+	public void afficherEtat() {
+		System.out.println("poids sac = " + P + " valeur sac =  " + V);
+	}
 }
